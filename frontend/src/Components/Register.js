698f0666406fe2e804ed5ccd5be6b67f6663d4d0
justinfoo9980 <justin.foo@ncs.com>
2024-04-17 00:00:15 +0800

    still CORS problem

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
-  const [formData, setFormData] = useState({ username: '', password: '', email: '' });
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -13,19 +17,21 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError('');
+    setError("");
 
     try {
-      // const response = await axios.post('http://localhost:3000/users', formData);
-      const responseTest = await axios.get('http://localhost:3000/users');
-      console.log(responseTest)
-      // console.log('Registered successfully:', response.data);
+      console.log(formData);
+      const response = await axios.post(
+        "https://calm-mountain-10484-296975bbfc99.herokuapp.com/users/registerUser",
+        formData
+      );
+      console.log("Registered successfully:", response.data);
       // Redirect or handle response
       setLoading(false);
       // Redirect to login or other appropriate page
     } catch (error) {
-      console.error('Error registering:', error);
-      setError('Failed to register. Please try again.');
+      console.error("Error registering:", error);
+      setError("Failed to register. Please try again.");
       setLoading(false);
     }
   };
@@ -33,12 +39,29 @@ function Register() {
   return (
     <form>
       <label>Username:</label>
-      <input type="text" name="username" value={formData.username} onChange={handleChange} />
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
       <label>Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
       <label>Password:</label>
-      <input type="password" name="password" value={formData.password} onChange={handleChange} />
-      <button type="submit" onClick={handleSubmit} disabled={loading}>{loading ? 'Registering...' : 'Register'}</button>
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button type="submit" onClick={handleSubmit} disabled={loading}>
+        {loading ? "Registering..." : "Register"}
+      </button>
       {error && <p>{error}</p>}
     </form>
   );
--- models/user.js
+++ models/user.js
@@ -1,48 +1,53 @@
-﻿const config = require('config');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const mongoose = require('mongoose');
+﻿const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 50
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 256
-    },
-    isAdmin: Boolean
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 256,
+  },
+  isAdmin: Boolean,
 });
 
-// userSchema.methods.generateAuthToken = function () {
-//     const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey').replace('<Bookstore_jwtPrivateKey>', process.env.APP_KEY));
-//     return token;
-// }
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config
+      .get("jwtPrivateKey")
+      .replace("<Bookstore_jwtPrivateKey>", process.env.APP_KEY)
+  );
+  return token;
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    // Input to the API from front end
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(50).required().email(),
-        password: Joi.string().min(5).max(256).required()
-    });
+  // Input to the API from front end
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(50).required().email(),
+    password: Joi.string().min(5).max(256).required(),
+  });
 
-    return schema.validate(user);
+  return schema.validate(user);
 }
 
-exports.userSchema = userSchema;//probaly can remove this once i test out using full schema
+exports.userSchema = userSchema; //probaly can remove this once i test out using full schema
 exports.User = User;
 exports.validate = validateUser;
