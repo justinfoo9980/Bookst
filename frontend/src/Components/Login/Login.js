4cfa7ab6124e716c128e3a1369db76013952e993
justinfoo9980 <justin.foo@ncs.com>
2024-04-20 18:38:08 +0800

    attempting to get user details

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 const error = "Error loggin in";
 function Login() {
   const [formData, setFormData] = useState({ email: "", password: "" });
-
+  const [loginMessage, setLoginMessage] = useState("");
+  const [error, setError] = useState("");
+  const [userName, setUserName] = useState("");
+  const [userToken, setUserToken] = useState("");
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      console.log("Token value:", storedToken);
+      // If token exists, redirect to dashboard or any other authenticated route
+      // Example: history.push("/dashboard");
+    }
+  }, []);
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -21,10 +32,26 @@ function Login() {
       const token = response.data.token;
       localStorage.setItem("token", token);
 
+      const userResponse = await axios.get(
+        "https://calm-mountain-10484-296975bbfc99.herokuapp.com/users/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const user = userResponse.data;
+      setUserName(user.name);
+
+      // Set login message
+      setLoginMessage(`Login successful. Welcome, ${user.name}.`);
+      // Reset error message
+      setError("");
+      console.log("Login successful:", user);
+
       // try to see if token is captured successful anot
-      console.log("Login Successful:", token);
+      console.log("Login Successful:", token); //delete later
     } catch (error) {
       console.error("Error logging in:", error);
+      setLoginMessage("Error logging in. Please try again.");
     }
   };
 
@@ -45,7 +72,9 @@ function Login() {
         onChange={handleChange}
       />
       <button type="submit">Login</button>
-      {error && <p>{error}</p>} {/* Display error message if present */}
+      {error && <p>{error}</p>} {""}
+      {loginMessage && <p>{loginMessage}</p>}{" "}
+      {/* Display login message if present */}
     </form>
   );
 }
